13b1fc8017f85bb7966df25dd1cbb271c7a757f7
Tygr <workfile975@gmail.com>
2019-11-02 09:38:58 +0800

    fix python3 path issue.

--- pcm2wav_sample.py
+++ pcm2wav_sample.py
@@ -6,10 +6,13 @@ class tygrlib:
     def pcm2wav(self,src_filename,dst_filename):
         res = self.lib.pcm2wav(src_filename,dst_filename)
 
-
+# work on python2 
 test_filename = "test10.pcm"
 output_filename = "test10.wav"
 
+# work on python3 
+# test_filename = "test10.pcm".encode('ascii')
+# output_filename = "test10.wav".encode('ascii')
 
 lib = tygrlib()
 lib.pcm2wav(test_filename,output_filename)
